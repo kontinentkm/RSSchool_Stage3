79dd793579ceaae2a13e6d3d57a0ff4fbf65ab56
Max <KontinentKM@gmail.com>
2023-10-27 08:35:48 +0300

    feat: search without state

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
-import { useState } from 'react';
-import { SearchBar } from './components/SearchBar/SearchBar';
+import React from 'react';
+import SearchBar from './components/SearchBar/SearchBar';
 import { SearchResultsList } from './components/SearchResultsList/SearchResultsList';
 import { Person } from './models';
 import './App.css';
 
-function App() {
-  const [results, setResults] = useState<Person[]>([]);
+interface AppProps {
+  results?: Person[];
+}
 
-  return (
-    <div className="App">
-      <div className="search-bar-container">
-        <SearchBar setResults={setResults} />
-        <SearchResultsList results={results} />
+interface AppState {
+  results: Person[];
+}
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {
+      results: [],
+    };
+  }
+
+  setResults = (results: Person[]) => {
+    this.setState({ results });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="search-bar-container">
+          <SearchBar setResults={this.setResults} />
+          <SearchResultsList results={this.state.results} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,44 +1,73 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import { Person } from '../../models';
 import './SearchBar.css';
 
-export const SearchBar = ({
-  setResults,
-}: {
+interface SearchBarProps {
   setResults: (results: Person[]) => void;
-}) => {
-  const [input, setInput] = useState('');
+}
 
-  const fetchData = (value: string) => {
+interface SearchBarState {
+  input: string;
+}
+
+class SearchBar extends Component<SearchBarProps, SearchBarState> {
+  constructor(props: SearchBarProps) {
+    super(props);
+    this.state = {
+      input: '',
+    };
+  }
+
+  fetchData = (value: string) => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((json) => {
         const results = json.filter((user: Person) => {
           return (
-            value &&
             user &&
             user.name &&
             user.name.toLowerCase().includes(value.toLowerCase())
           );
         });
-        setResults(results);
+        this.props.setResults(results);
       });
   };
 
-  const handleChange = (value: string) => {
-    setInput(value);
-    fetchData(value);
+  handleChange = (value: string) => {
+    this.setState({ input: value });
   };
 
-  return (
-    <div className="input_wrapper">
-      <FaSearch id="search-icon" />
-      <input
-        placeholder="Type to search..."
-        value={input}
-        onChange={(e) => handleChange(e.target.value)}
-      />
-    </div>
-  );
-};
+  handleSearch = () => {
+    this.fetchData(this.state.input);
+  };
+
+  handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      this.handleSearch();
+    }
+  };
+
+  componentDidMount() {
+    this.fetchData('');
+  }
+
+  render() {
+    return (
+      <div className="input_wrapper">
+        <FaSearch id="search-icon" />
+        <input
+          placeholder="..."
+          value={this.state.input}
+          onChange={(e) => this.handleChange(e.target.value)}
+          onKeyPress={this.handleKeyPress}
+        />
+        <button className="search-button" onClick={this.handleSearch}>
+          Search
+        </button>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
